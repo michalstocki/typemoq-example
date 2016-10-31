bd7b3efdfed247a91825f0b74c382724a431df65
Michał Stocki <mstocki@ydp.eu>
2016-10-31 19:31:50 +0100

    Minimal tech stack to run typescript tests using a mocha framework

---
+++ test/spec/pagination/PaginationItemsProvider.spec.ts
@@ -0,0 +1,19 @@
+import {PaginationItemsProvider} from '../../../src/pagination/PaginationItemsProvider';
+
+describe('PaginationItemsProvider', () => {
+  let paginationItemsProvider:PaginationItemsProvider;
+
+  beforeEach(() => {
+    paginationItemsProvider = new PaginationItemsProvider();
+  });
+
+  describe('What?', () => {
+    it('Hello!', () => {
+      // given
+
+      // when
+
+      // then
+    });
+  });
+});
